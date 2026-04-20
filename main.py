76b3f586873c3847df073eec22fe8bc24ebe95a1
mchurakov-5512 <mchurakov777@gmail.com>
2026-04-20 16:38:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import cv2
 import numpy as np
 from deepface import DeepFace
 
+
+owner_face = "owner.jpg"
+
+result = DeepFace.verify(img1_path=owner_face,
+                         img2_path="face_from_camera.jpg",
+                         model_name="Facenet")
+
+if result["verified"]:
+    print("Здравствуйте, хозяин")
+else:
+    print("Лицо не распознано")
+
